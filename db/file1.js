bbba5b846c9fdb125fa3637f83f3551b4f4d9eeb
percivalho <35653644+percivalho@users.noreply.github.com>
2023-07-02 22:32:58 +0100

    fixing file1/2 issue

--- db/file1.js
+++ db/file1.js
@@ -11,10 +11,30 @@ fs.readFile(sqlFilePath, 'utf8', function(err, data) {
     if (err) throw err;
     console.log('Loaded SQL file');
 
-    connection.query(data, function(err, results) {
-        if (err) throw err;
-        console.log('Executed SQL file');
+    // Split data into individual queries
+    var queries = data.split(';').map(function(query) {
+        return query.trim();  // Trim whitespace
+    }).filter(function(query) {
+        return query.length > 0;  // Filter out empty queries
     });
 
-    connection.end();
+    // Execute each query one by one
+    var index = 0;
+    function executeNextQuery() {
+        if (index >= queries.length) {
+            console.log('Executed all SQL commands');
+            connection.end();
+            return;
+        }
+
+        var query = queries[index++];
+        console.log('Executing query:', query);
+        connection.query(query, function(err, results) {
+            if (err) throw err;
+            console.log('Executed SQL command');
+            executeNextQuery();  // Proceed to next query
+        });
+    }
+
+    executeNextQuery();  // Start executing queries
 });
